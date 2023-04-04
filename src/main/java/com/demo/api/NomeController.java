8d11d3083ce8b1010025dba2fd39cbe1c7b4b3c5
Elton Goncalves <elton@dobemcontabilidade.com>
2023-04-04 01:21:06 -0300

    update nomes

--- src/main/java/com/demo/api/NomeController.java
+++ src/main/java/com/demo/api/NomeController.java
@@ -14,14 +14,7 @@ public class NomeController {
 	@GetMapping(value= "nomes")
 	public List<String> getNomes() {
 		var lista =  new ArrayList<String>();
-		lista.add("Edevaldo Pinheiro");
-		lista.add("Creuza Goncalves");
-		lista.add("Edkeyze Goncalves");
-		lista.add("Elton Goncalves");
-		lista.add("Alessandra Goncalves");
-		lista.add("Beatriz Goncalves");		
-		lista.add("Maria Andrade Goncalves");	
-		lista.add("Mais um nome Goncalves");			
+		lista.add("A");				
 		return lista;
 	}
 
